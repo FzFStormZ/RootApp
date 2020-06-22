f85556054338a190188e2f66c6217ee284ef1f6c
FzFStormZ <dylaa.degail@gmail.com>
2020-06-23 00:36:53 +0200

    Connection

---
+++ RootApp/RootApp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RootApp
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient connection;
+        private HttpResponseMessage response;
+
+        private string cookie;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _ = Connection();
+        }
+
+        public async Task Connection()
+        {
+            try
+            {
+                connection = new HttpClient();
+                response = await connection.GetAsync(ConfigurationManager.ConnectionStrings["root-me"].ConnectionString);
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                string[] delimiter = { ", " };
+
+                cookie = responseContent.Replace('}', ' ').Replace(']', ' ').Split(delimiter, StringSplitOptions.RemoveEmptyEntries)[2];
+                Console.WriteLine(cookie);
+
+            } catch (HttpRequestException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public async Task Challenges(int num) 
+        {
+            "https://api.www.root-me.org/challenges/" + num;
+        }
+    }
+}
